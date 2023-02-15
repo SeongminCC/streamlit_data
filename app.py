e373c1134d11a2ed734e8dfc72cd6782eb233ae9
SeongminCC <110529690+SeongminCC@users.noreply.github.com>
2023-02-16 05:12:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 import streamlit as st
+import joblib
 import pandas as pd
-import lightgbm as lgb
-import torch
+from SMCluster import smC_v2
 
+# Load the model
+loaded_model = joblib.load('model.txt')
+target = ['B형간염', '뇌졸중', '협심증또는심근경색증', '고혈압&당뇨']
+
+# Create the main function
 def main():
-    st.title("Main Page")
-    st.write("Welcome to the Main Pageaaa.")
+    st.title('LightGBM Model Predictor')
 
-    user_input = st.text_input("Enter a number:")
+    # Get the user inputs
+    HE_glu = st.number_input(smC_v2().trans('HE_glu'), value=0.0)
+    HE_dbp = st.number_input(smC_v2().trans('HE_dbp'), value=0.0)
+    HE_sbp = st.number_input(smC_v2().trans('HE_sbp'), value=0.0)
 
-    if user_input:
-        user_input = int(user_input)
-        result = user_input * 2
+    st.write('Disease : B형간염 / 뇌졸중 / 협심증또는심근경색증 / 고혈압&당뇨')
 
-        if st.button("Double"):
-            st.write(result)
+    # Predict the target
+    if st.button('Predict'):
+        prediction = loaded_model.predict([[HE_glu, HE_dbp, HE_sbp]])
+        st.write('The predicted target is:', target[int(prediction)])
 
+# Run the main function
 if __name__ == '__main__':
     main()
