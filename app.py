5d65a0fef5d95142fe098f3fc84bbc3404194c90
SeongminCC <110529690+SeongminCC@users.noreply.github.com>
2023-02-16 03:50:19 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas_datareader as pdr
+
+import streamlit as st
+
+import streamlit as st
+
+def main():
+    st.title("Main Page")
+    st.write("Welcome to the Main Page.")
+
+    user_input = st.text_input("Enter a number:")
+
+    if user_input:
+        user_input = int(user_input)
+        result = user_input * 2
+
+        if st.button("Double"):
+            st.write(result)
+
+if __name__ == '__main__':
+    main()
