7e0fd7b435ef64e15836ea1cf1bb40ce326852a4
SeongminCC <110529690+SeongminCC@users.noreply.github.com>
2023-02-16 13:02:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,15 @@
 import streamlit as st
-import joblib
-import pandas as pd
-from SMCluster import smC_v2
 
-# Load the model
-loaded_model = joblib.load('model.txt')
-target = ['B형간염', '뇌졸중', '협심증또는심근경색증', '고혈압&당뇨']
+
+
 
 # Create the main function
 def main():
-    st.title('LightGBM Model Predictor')
+    st.write("hello")
 
-    # Get the user inputs
-    HE_glu = st.number_input(smC_v2().trans('HE_glu'), value=0.0)
-    HE_dbp = st.number_input(smC_v2().trans('HE_dbp'), value=0.0)
-    HE_sbp = st.number_input(smC_v2().trans('HE_sbp'), value=0.0)
 
-    st.write('Disease : B형간염 / 뇌졸중 / 협심증또는심근경색증 / 고혈압&당뇨')
 
-    # Predict the target
-    if st.button('Predict'):
-        prediction = loaded_model.predict([[HE_glu, HE_dbp, HE_sbp]])
-        st.write('The predicted target is:', target[int(prediction)])
+
 
 # Run the main function
 if __name__ == '__main__':
