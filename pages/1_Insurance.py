abc56d415ff72dc35df0f84468a1571daec5473a
SeongminCC <110529690+SeongminCC@users.noreply.github.com>
2023-02-17 11:40:03 +0900

    Update 1_Insurance.py

--- pages/1_Insurance.py
+++ pages/1_Insurance.py
@@ -10,6 +10,7 @@ import numpy as np
 prediction_proba = np.load('prediction_proba.npy')
 prediction_proba = prediction_proba.tolist()[0]
 insurance_premium=pd.read_excel('./data/insurance_premium.xlsx')
+insurance_premium_info=pd.read_excel('./data/insurance_premium_info.xlsx')
 
 st.write("# Here is your disease-specific insurance policy information. \n **Please check the following.**")
 st.write('---')
@@ -22,26 +23,25 @@ insurance_amount = st.number_input("Please enter the desired insurance amount", 
 
 if st.button("Submit"):
     st.write("### DATA for INSURANCE:")
-    st.dataframe(insurance_premium)
+    st.dataframe(insurance_premium_info)
     st.write('#### RESULT')
     st.write('---')
 
 
     if 19<age<30 or 39<age<50:
-        list_1, list_2, list_3=Insurance_function.calculator(insurance_premium, prediction_proba, age, insurance_amount)
+        ipc_df=Insurance_function.calculator(insurance_premium, prediction_proba, age,insurance_amount)
         
-        if sum(list_2)==0:
-            st.write('설정한 최소 보험료가 너무 낮습니다. 계산된 최소 보험료 : %d원'%sum(list_3))
+        if ipc_df.iloc[5,0]=='0원':
+            print('설정한 최소 보험료가 너무 낮습니다. 계산된 최소 보험료 : %s'%ipc_df.iloc[5,1])
             
         else:
-            for i in range(len(list_1)):
-                st.write(f"**{list_1[i]} : {int(list_2[i])}원 / {int(list_3[i])}원**")
-            st.write('---')
-            st.write(f"**보험료 총합 : {int(sum(list_2))}원**")
+            st.dataframe(ipc_df)
         
     else:
-        st.write('죄송합니다. 현재 고객님을 위한 상품이 준비되어 있지 않습니다.')
-        st.write('현재 저희는 20대와 40대를 위한 상품만 제공되고 있습니다.')
+        print('죄송합니다. 현재 고객님을 위한 상품이 준비되어 있지 않습니다.')
+        print('현재 저희는 20대와 40대를 위한 상품만 제공되고 있습니다.')
+    
+
 
     #### 보험 #### 
 
